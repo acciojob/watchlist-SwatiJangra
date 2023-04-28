ee80c9e95766234343eef0bce0d7e6b837e9186b
Swati Jangra <111527689+SwatiJangra@users.noreply.github.com>
2023-04-29 03:22:00 +0530

    Update Movie.java

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,41 +1,17 @@
 package com.driver;
 
-public class Movie {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Movie {
     private String name;
     private int durationInMinutes;
     private double imdbRating;
 
-    public Movie(){
-
-    }
-    public Movie(String name, int durationInMinutes, double imdbRating) {
-        this.name = name;
-        this.durationInMinutes = durationInMinutes;
-        this.imdbRating = imdbRating;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDurationInMinutes() {
-        return durationInMinutes;
-    }
-
-    public void setDurationInMinutes(int durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
-    }
-
-    public double getImdbRating() {
-        return imdbRating;
-    }
-
-    public void setImdbRating(double imdbRating) {
-        this.imdbRating = imdbRating;
-    }
 }
